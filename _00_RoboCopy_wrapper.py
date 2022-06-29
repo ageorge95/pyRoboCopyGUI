dcc6ff3c769e0ed70fe3d44f479ced35d1cb1666
George Daniel Arteni <arteni.george.daniel@gmail.com>
2022-06-29 19:08:11 +0300

    refinements and bufixes for RoboCopyWrapper

--- _00_RoboCopy_wrapper.py
+++ _00_RoboCopy_wrapper.py
@@ -27,24 +27,28 @@ class RoboCopyWrapper():
         self.ipg_str = str(self.ipg)
 
         if self.move:
-            self.move_str = r'/MOVE'
+            if path.isdir(self.input):
+                self.move_str = r'/MOVE'
+            else:
+                self.move_str = r'/mov'
         else:
             self.move_str = ''
 
     def sanity_check(self) -> Dict:
         if not any([path.isdir(self.input), path.isfile(self.input)]):
             return {'success': False,
-                    'reason': f"{self.input} is neither a file nor a dir"}
-        if not path.isdir(self.output):
+                    'reason': f"INPUT {self.input} is neither a file nor a dir"}
+        if not path.isdir(path.dirname(self.output)):
             return {'success': False,
-                    'reason': f"{self.output} is not a dir"}
+                    'reason': f"OUTPUT {self.output} is not a dir"}
         return {'success': True,
                 'reason': None}
 
     def return_full_CLI_call_str(self):
         return 'robocopy' + \
+               (' /E' if path.isdir(self.input) else '') + \
                f' "{self.input_path_str}"' + \
                f' "{self.output}"' + \
                (f' "{self.input_file_str}"' if self.input_file_str else '') + \
                f' {self.move_str}' + \
-               f' /IPG {self.ipg_str}'
+               f' /IPG:{self.ipg_str}'
