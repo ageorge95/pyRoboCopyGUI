df8bba9887a3496638f50cd248d7055c5390548d
ageorge95 <arteni.george.daniel@gmail.com>
2023-10-15 11:06:21 +0300

    added a proper Install.bat and requirements.txt

---
+++ Install.bat
@@ -0,0 +1,29 @@
+@echo off
+
+set root=%cd%
+
+:: installation
+
+python -m venv venv
+:: Windows doesn't allow the creation of symlinks without special priviledges, so hardlinks are created instead.
+mklink /h activate.bat venv\Scripts\activate.bat
+
+call activate.bat
+
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+
+:: post-installation message
+
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo.
+echo pyRobocopyGUI install complete.
+echo.
+echo Run 'activate' to activate pyRobocopyGUI's Python virtual environment and
+echo 'deactivate' to, well, deactivate it.
+echo.
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+pause
+
+deactivate
